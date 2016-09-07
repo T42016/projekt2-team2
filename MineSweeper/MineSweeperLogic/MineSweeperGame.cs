906758349e58034fb63ad8535f06fd0fed21a05d
Pontus <pontus.fogelberg@lund.ntig.se>
2016-09-07 10:01:10 +0200

    Minesweeper DrawBoard

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,7 @@ namespace MineSweeperLogic
         private int sizeY;
         private int numberOfMines;
         private GameState state;
+        public IServiceBus bus;
 
         public MineSweeperGame(int sizeX, int sizeY, int numberOfMines, IServiceBus bus)
         {
@@ -24,14 +26,17 @@ namespace MineSweeperLogic
             SizeY = sizeY;
             NumberOfMines = numberOfMines;
             State = state;
+            Bus = bus;
         }
 
+        public ConsoleColor DarkCyan { get; set; }
         public int PosX { get; private set; }
         public int PosY { get; private set; }
         public int SizeX { get; }
         public int SizeY { get; }
         public int NumberOfMines { get; }
         public GameState State { get; private set; }
+        public IServiceBus Bus { get; }
 
         public PositionInfo GetCoordinate(int x, int y)
         {
@@ -55,7 +60,49 @@ namespace MineSweeperLogic
 
         public void DrawBoard()
         {
+            Console.WriteLine();
+            for (int i = 0; i < SizeY; i++)
+            {
+                
+                for (int j = 0; j < SizeX; j++)
+                {
+                    
 
+                    if (j == PosX && i == PosY)
+                    {
+                        Bus.Write("O ", ConsoleColor.Blue);
+                    }
+                    else
+                    {
+                        Bus.Write("? ", ConsoleColor.DarkCyan);
+                    }
+                }
+                Console.WriteLine();
+                
+            }
+            Bus.Write("! ");
+            //Bus.Write("? ", ConsoleColor.DarkCyan);
+            //Bus.Write("X ", ConsoleColor.DarkCyan);
+            //Bus.Write("! ", ConsoleColor.DarkCyan);
+            //Bus.Write("2 ", ConsoleColor.DarkCyan);
+            //Bus.Write(". ", ConsoleColor.DarkCyan);
+            //Bus.Write(". ");
+            //Bus.Write(". ");
+            //Bus.Write(". ");
+            //Bus.Write("1 ");
+            //Bus.Write("2 ");
+            //Bus.Write("3 ");
+            //Bus.Write("4 ");
+            //Bus.Write("5 ");
+            //Bus.Write("6 ");
+            //Bus.Write("7 ");
+            //Bus.Write("8 ");
+            //Bus.Write("? ");
+            //Bus.Write("? ");
+            //Bus.Write("? ");
+            //Bus.Write("! ");
+            //Bus.Write("X ");
+            //Console.WriteLine();
         }
 
         #region MoveCursor Methods
